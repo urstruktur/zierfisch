592e47a4c6b343aaa833bf3f08dc02c011979cec
urstruktur <s1310238052@students.fh-hagenberg.at>
2016-12-06 16:56:12 +0100

    converted mesh to only tris + test obj import

--- src/main/java/com/zierfisch/test/ObjImport.java
+++ src/main/java/com/zierfisch/test/ObjImport.java
@@ -1,8 +1,11 @@
 package com.zierfisch.test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.owens.oobjloader.builder.Build;
+import com.owens.oobjloader.builder.Face;
+import com.owens.oobjloader.builder.FaceVertex;
 import com.owens.oobjloader.parser.Parse;
 
 public class ObjImport {
@@ -10,6 +13,9 @@ public class ObjImport {
 	    Build builder = new Build();
 	    try {
 			Parse obj = new Parse(builder, "assets/models/pascal.obj");
+			ArrayList<Face> faces = builder.faces;
+			System.out.println("Loaded " + builder.objectName);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
