ed6d65520147e750c5dfec4b44151c4e5e43489d
Philipp Stadler <hello@phstadler.com>
2016-12-06 17:09:57 +0100

    Added vbo

---
+++ src/main/java/com/zierfisch/vbo/VBO.java
@@ -0,0 +1,40 @@
+package com.zierfisch.vbo;
+
+import static org.lwjgl.opengl.GL15.*;
+
+public class VBO {
+
+	private int name = Integer.MIN_VALUE;
+	private int target;
+	
+	private void verifyHasContents() {
+		if(name == Integer.MIN_VALUE) {
+			throw new RuntimeException("Trying to use buffer with no contents");
+		}
+	}
+	
+	public int getName() {
+		verifyHasContents();
+		return name;
+	}
+	
+	public void bind() {
+		verifyHasContents();
+		glBindBuffer(target, name);
+	}
+	
+	public void setContents(float[] data) {
+		name = glGenBuffers();
+		target = GL_ARRAY_BUFFER;
+		glBindBuffer(target, name);
+        glBufferData(name, data, GL_STATIC_DRAW);
+	}
+	
+	public void setContents(short[] data) {
+		name = glGenBuffers();
+		target = GL_ELEMENT_ARRAY_BUFFER;
+		glBindBuffer(target, name);
+        glBufferData(name, data, GL_STATIC_DRAW);
+	}
+
+}
