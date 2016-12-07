ee0e02200858fddb4d4acf2b13872750f1de0407
Philipp Stadler <hello@phstadler.com>
2016-12-07 15:17:08 +0100

    Added uniform setters

--- src/main/java/com/zierfisch/shader/Shader.java
+++ src/main/java/com/zierfisch/shader/Shader.java
@@ -1,14 +1,36 @@
 package com.zierfisch.shader;
 
-import xyz.krachzack.gfx.mesh.Mesh;
-import xyz.krachzack.gfx.mesh.Primitive;
-import xyz.krachzack.gfx.mesh.VertexAttribute;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glGetAttribLocation;
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
+import static org.lwjgl.opengl.GL20.glUniform1f;
+import static org.lwjgl.opengl.GL20.glUniform1i;
+import static org.lwjgl.opengl.GL20.glUniform2f;
+import static org.lwjgl.opengl.GL20.glUniform3f;
+import static org.lwjgl.opengl.GL20.glUniform4f;
+import static org.lwjgl.opengl.GL20.glUniformMatrix4fv;
+import static org.lwjgl.opengl.GL20.glUseProgram;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL20.*;
+import java.nio.FloatBuffer;
+
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+import org.lwjgl.BufferUtils;
 
 import com.zierfisch.util.GLErrors;
 import com.zierfisch.vbo.VBO;
+
+import xyz.krachzack.gfx.mesh.Mesh;
+import xyz.krachzack.gfx.mesh.Primitive;
+import xyz.krachzack.gfx.mesh.VertexAttribute;
 
 /**
  * Represents a shader program.
@@ -16,6 +38,8 @@ import com.zierfisch.vbo.VBO;
  * @author phil
  */
 public class Shader {
+	
+	private static FloatBuffer tmp4x4Buf = BufferUtils.createFloatBuffer(4*4);
 
 	private int name;
 	
@@ -25,6 +49,68 @@ public class Shader {
 	
 	public void bind() {
 		glUseProgram(name);
+	}
+	
+	public void setUniform(int location, int value) {
+		glUniform1i(location, value);
+	}
+	
+	public void setUniform(int location, float value) {
+		glUniform1f(location, value);
+	}
+	
+	public void setUniform(int location, Vector2f value) {
+		glUniform2f(location, value.x, value.y);
+	}
+	
+	public void setUniform(int location, Vector3f value) {
+		glUniform3f(location, value.x, value.y, value.z);
+	}
+	
+	public void setUniform(int location, Vector4f value) {
+		glUniform4f(location, value.x, value.y, value.z, value.w);
+	}
+	
+	public void setUniform(int location, Matrix4f value) {
+		tmp4x4Buf.clear();
+		glUniformMatrix4fv(location, false, value.get(tmp4x4Buf));
+	}
+	
+	public void setUniform(int location, Matrix3f value) {
+		tmp4x4Buf.clear();
+		glUniformMatrix4fv(location, false, value.get(tmp4x4Buf));
+	}
+	
+	public void setUniform(String uniformName, int value) {
+		setUniform(getUniformLocation(uniformName), value);
+	}
+	
+	public void setUniform(String uniformName, float value) {
+		setUniform(getUniformLocation(uniformName), value);
+	}
+	
+	public void setUniform(String uniformName, Vector2f value) {
+		setUniform(getUniformLocation(uniformName), value);
+	}
+	
+	public void setUniform(String uniformName, Vector3f value) {
+		setUniform(getUniformLocation(uniformName), value);
+	}
+	
+	public void setUniform(String uniformName, Vector4f value) {
+		setUniform(getUniformLocation(uniformName), value);
+	}
+	
+	public void setUniform(String uniformName, Matrix3f value) {
+		setUniform(getUniformLocation(uniformName), value);
+	}
+	
+	public void setUniform(String uniformName, Matrix4f value) {
+		setUniform(getUniformLocation(uniformName), value);
+	}
+	
+	public int getUniformLocation(String uniformName) {
+		return glGetUniformLocation(name, uniformName);
 	}
 	
 	public int getAttributeLocation(String attrName) {
