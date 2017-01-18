98811fbc81e74e69005977a6282bc6afbc47d526
Philipp Stadler <hello@phstadler.com>
2017-01-18 13:16:55 +0100

    Added MusicSystem that plays the soundtrack

---
+++ src/main/java/com/zierfisch/audio/MusicSystem.java
@@ -0,0 +1,31 @@
+package com.zierfisch.audio;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.EntitySystem;
+
+public class MusicSystem extends EntitySystem {
+
+	private static final String SOUNDTRACK_WAV_FILE = "assets/audio/non.wav";
+	
+	private GhettoBlaster blaster;
+	private boolean playing;
+	
+	@Override
+	public void addedToEngine(Engine engine) {
+		super.addedToEngine(engine);
+		
+		blaster = new GhettoBlaster();
+		blaster.load(SOUNDTRACK_WAV_FILE);
+		
+		playing = false;
+	}
+	
+	@Override
+	public void update(float deltaTime) {
+		if(!playing) {
+			blaster.play();
+			playing = true;
+		}
+	}
+	
+}
